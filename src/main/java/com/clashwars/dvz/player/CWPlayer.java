b19b84bb3d9150d0fb15c1cc232162c9342ca17c
WarmakerT <WarmakerT@gmail.com>
2014-10-26 01:42:59 +0100

    Basic CWPlayer/PlayerData code.

--- src/main/java/com/clashwars/dvz/player/CWPlayer.java
+++ src/main/java/com/clashwars/dvz/player/CWPlayer.java
@@ -1,6 +1,22 @@
 package com.clashwars.dvz.player;
 
+import com.clashwars.dvz.classes.DvZClass;
+import org.bukkit.entity.Player;
+
+import java.util.UUID;
+
 public class CWPlayer {
 
+    private Player player;
+    private PlayerData pd;
+
+    public CWPlayer(Player player, DvZClass playerClass, int progress) {
+        this.player = player;
+        this.pd = new PlayerData(player, playerClass, progress);
+    }
+
+    public PlayerData getPlayerData(){
+        return pd;
+    }
 
 }
--- src/main/java/com/clashwars/dvz/player/PlayerData.java
+++ src/main/java/com/clashwars/dvz/player/PlayerData.java
@@ -1,5 +1,31 @@
 package com.clashwars.dvz.player;
 
+import com.clashwars.dvz.classes.ClassType;
+import com.clashwars.dvz.classes.DvZClass;
+import org.bukkit.entity.Player;
+
 public class PlayerData {
+
+    private Player player;
+    private DvZClass playerClass;
+    private int progress;
+
+    public PlayerData(Player player, DvZClass playerClass, int progress) {
+        this.player = player;
+        this.playerClass = playerClass;
+        this.progress = progress;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public DvZClass getPlayerClass() {
+        return playerClass;
+    }
+
+    public int getProgress() {
+        return progress;
+    }
 
 }
