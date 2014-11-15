b4c1e58e3e5e307f0c206ed3f935dbc46e6eaa93
WarmakerT <WarmakerT@gmail.com>
2014-11-15 14:20:07 +0000

    % Fixed Blink teleportation.

--- src/main/java/com/clashwars/dvz/abilities/monsters/Blink.java
+++ src/main/java/com/clashwars/dvz/abilities/monsters/Blink.java
@@ -5,6 +5,7 @@ import com.clashwars.dvz.util.DvzItem;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -21,7 +22,7 @@ public class Blink extends MobAbility {
     public void castAbility(Player player, Location triggerLoc) {
         Block b = player.getTargetBlock(null, 50);
         if(b != null) {
-            player.teleport(b.getLocation());
+            player.teleport(b.getLocation().getBlock().getRelative(BlockFace.UP).getLocation());
         }
     }
 
