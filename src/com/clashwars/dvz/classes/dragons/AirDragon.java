cedc5ab212e160416680c78be78d0bdc9f8a8f1c
Ryan Harris <kadowster@gmail.com>
2015-04-29 19:29:36 +0100

    +Added WindStorm ability for the Air Dragon

--- src/com/clashwars/dvz/classes/dragons/AirDragon.java
+++ src/com/clashwars/dvz/classes/dragons/AirDragon.java
@@ -1,5 +1,6 @@
 package com.clashwars.dvz.classes.dragons;
 
+import com.clashwars.dvz.abilities.Ability;
 import com.clashwars.dvz.classes.DvzClass;
 
 public class AirDragon extends DragonClass {
@@ -7,6 +8,8 @@ public class AirDragon extends DragonClass {
     public AirDragon() {
         super();
         dvzClass = DvzClass.AIRDRAGON;
+
+        abilities.add(Ability.WINDSTORM);
     }
 
 }
