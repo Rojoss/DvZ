e8e05f9862a5b6e41cee61b253c802bb3bb7f406
Jos <josroossien@hotmail.com>
2015-05-08 18:54:13 +0200

    %Finished sorting for getting locations in cone. +Added method to get entities in cone.

--- src/com/clashwars/dvz/abilities/dragons/Wind.java
+++ src/com/clashwars/dvz/abilities/dragons/Wind.java
@@ -2,18 +2,23 @@ package com.clashwars.dvz.abilities.dragons;
 
 import com.clashwars.cwcore.Debug;
 import com.clashwars.cwcore.packet.ParticleEffect;
+import com.clashwars.cwcore.utils.CWUtil;
 import com.clashwars.cwcore.utils.VectorUtils;
 import com.clashwars.dvz.abilities.Ability;
 import com.clashwars.dvz.util.DvzItem;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Sound;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Wind extends DragonAbility {
@@ -34,15 +39,31 @@ public class Wind extends DragonAbility {
 
         new BukkitRunnable() {
             int index = 0;
+            int particles = 1;
 
             @Override
             public void run() {
-                ParticleEffect.CLOUD.display(0.2f, 0.2f, 0.2f, 0f, 1, locs.get(index).toLocation(player.getWorld()), 500);
-                index++;
-                if (index >= locs.size()) {
-                    cancel();
-                    return;
+                for (int i = 0; i < particles; i++) {
+                    Location loc = locs.get(index).toLocation(player.getWorld());
+
+                    List<Entity> entities = CWUtil.getNearbyEntities(loc, 1f, null);
+                    for (Entity e : entities) {
+                        if (!(e instanceof LivingEntity) || e == player) {
+                            continue;
+                        }
+                        ((LivingEntity)e).damage(2);
+                        e.setVelocity(e.getVelocity().add( player.getLocation().getDirection().multiply(2) ));
+                    }
+
+                    ParticleEffect.CLOUD.display(0.1f, 0.1f, 0.1f, 0f, 1, loc, 500);
+
+                    index++;
+                    if (index >= locs.size()) {
+                        cancel();
+                        return;
+                    }
                 }
+                particles += 3;
             }
         }.runTaskTimer(dvz, 1, 1);
     }
