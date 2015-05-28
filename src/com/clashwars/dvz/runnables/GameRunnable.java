8d65afbfd1c6378b38c95f083d3b80d2b2a91bd7
Jos <josroossien@hotmail.com>
2015-05-28 20:00:08 +0200

    +Decay system for the shrine. (If there aren't enough dwarves nearby it will start taking damage every 3 seconds)

--- src/com/clashwars/dvz/runnables/GameRunnable.java
+++ src/com/clashwars/dvz/runnables/GameRunnable.java
@@ -2,13 +2,28 @@ package com.clashwars.dvz.runnables;
 
 import com.clashwars.cwcore.Debug;
 import com.clashwars.cwcore.packet.ParticleEffect;
+import com.clashwars.cwcore.utils.CWUtil;
 import com.clashwars.dvz.DvZ;
 import com.clashwars.dvz.GameManager;
 import com.clashwars.dvz.GameState;
+import com.clashwars.dvz.classes.ClassType;
+import com.clashwars.dvz.maps.ShrineBlock;
+import com.clashwars.dvz.maps.ShrineType;
+import com.clashwars.dvz.player.CWPlayer;
 import com.clashwars.dvz.util.Util;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+//This runnable runs half the speed! (2 tick delay)
 public class GameRunnable extends BukkitRunnable {
 
     private DvZ dvz;
@@ -23,22 +38,61 @@ public class GameRunnable extends BukkitRunnable {
 
     @Override
     public void run() {
+        ticks++;
         if (!gm.isStarted()) {
             return;
         }
+
+        //Dragonslayer effect
         if (gm.getDragonSlayer() != null) {
             ParticleEffect.SPELL_WITCH.display(0.2f, 0.4f, 0.2f, 0.01f, 5, gm.getDragonSlayer().getLocation());
         }
 
-        ticks++;
-        if (ticks >= 1200) {
+        //Saving
+        if (ticks % 600 == 0) {
+            Bukkit.broadcastMessage(Util.formatMsg("&7Saving all data..."));
             dvz.getPM().savePlayers();
-            ticks = 0;
         }
 
         if (gm.isMonsters()) {
+            //Shrine damage
+            if (ticks % 30 != 0) {
+                return;
+            }
+            Set<ShrineBlock> shrineBlocks = new HashSet<ShrineBlock>();
+            Location center = null;
+            if (dvz.getGM().getState() == GameState.MONSTERS) {
+                shrineBlocks = dvz.getGM().getShrineBlocks(ShrineType.WALL);
+                center = dvz.getMM().getActiveMap().getCuboid("shrinewall").getCenterLoc();
+            } else if (dvz.getGM().getState() == GameState.MONSTERS_WALL) {
+                shrineBlocks = dvz.getGM().getShrineBlocks(ShrineType.KEEP_1);
+                center = dvz.getMM().getActiveMap().getCuboid("shrine1keep").getCenterLoc();
+            } else if (dvz.getGM().getState() == GameState.MONSTERS_KEEP) {
+                shrineBlocks = dvz.getGM().getShrineBlocks(ShrineType.KEEP_1);
+                center = dvz.getMM().getActiveMap().getCuboid("shrine2keep").getCenterLoc();
+            }
+
+            if (shrineBlocks != null && center != null && shrineBlocks.size() > 0) {
+                List<Entity> nearbyEntities = CWUtil.getNearbyEntities(center, 20f, Arrays.asList(new EntityType[] {EntityType.PLAYER}));
+                int dwarfCount = 0;
+                for (Entity e : nearbyEntities) {
+                    CWPlayer cwp = dvz.getPM().getPlayer((Player)e);
+                    if (cwp.isDwarf()) {
+                        dwarfCount++;
+                    }
+                }
+                if (dwarfCount <= Math.round(dvz.getPM().getPlayers(ClassType.DWARF, true).size() * 0.3f)) {
+                    for (ShrineBlock shrineBlock : shrineBlocks) {
+                        if (shrineBlock != null && !shrineBlock.isDestroyed()) {
+                            shrineBlock.damage();
+                        }
+                    }
+                }
+            }
             return;
         }
+
+        //Game loop
         Long time = gm.getUsedWorld().getTime();
         if (gm.getSpeed() != 0) {
             gm.getUsedWorld().setTime(time + gm.getSpeed());
