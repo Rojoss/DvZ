ade310043af1666842fa1dfa7c3b75a9f8c5101d
Jos <josroossien@hotmail.com>
2015-05-30 19:45:13 +0200

    +Add message if people right click class item.

--- src/com/clashwars/dvz/events/MainEvents.java
+++ src/com/clashwars/dvz/events/MainEvents.java
@@ -318,38 +318,41 @@ public class MainEvents implements Listener {
         }
 
         //Check for class item usage.
-        if (event.getAction() == Action.LEFT_CLICK_BLOCK || event.getAction() == Action.LEFT_CLICK_AIR) {
-            for (DvzClass dvzClass : DvzClass.values()) {
-                BaseClass c = dvzClass.getClassClass();
-                if (c == null || c.getClassItem() == null || c.getClassItem().getType() != item.getType()) {
+        for (DvzClass dvzClass : DvzClass.values()) {
+            BaseClass c = dvzClass.getClassClass();
+            if (c == null || c.getClassItem() == null || c.getClassItem().getType() != item.getType()) {
+                continue;
+            }
+            if ((c.getClassItem().hasItemMeta() && !item.hasItemMeta()) || (!c.getClassItem().hasItemMeta() && item.hasItemMeta())) {
+                continue;
+            }
+            if (item.hasItemMeta()) {
+                if ((c.getClassItem().getItemMeta().hasDisplayName() && !item.getItemMeta().hasDisplayName()) || (!c.getClassItem().getItemMeta().hasDisplayName() && item.getItemMeta().hasDisplayName())) {
                     continue;
                 }
-                if ((c.getClassItem().hasItemMeta() && !item.hasItemMeta()) || (!c.getClassItem().hasItemMeta() && item.hasItemMeta())) {
-                    continue;
-                }
-                if (item.hasItemMeta()) {
-                    if ((c.getClassItem().getItemMeta().hasDisplayName() && !item.getItemMeta().hasDisplayName()) || (!c.getClassItem().getItemMeta().hasDisplayName() && item.getItemMeta().hasDisplayName())) {
+                if (item.getItemMeta().hasDisplayName()) {
+                    if (!CWUtil.integrateColor(c.getDisplayName()).equalsIgnoreCase(CWUtil.integrateColor(item.getItemMeta().getDisplayName()))) {
                         continue;
                     }
-                    if (item.getItemMeta().hasDisplayName()) {
-                        if (!CWUtil.integrateColor(c.getDisplayName()).equalsIgnoreCase(CWUtil.integrateColor(item.getItemMeta().getDisplayName()))) {
-                            continue;
-                        }
-                    }
                 }
-                if (!dvz.getGM().isStarted()) {
-                    player.sendMessage(Util.formatMsg("&cThe game hasn't started yet!"));
-                    break;
-                }
-                if (dvzClass.getType() == ClassType.MONSTER && !dvz.getGM().isMonsters()) {
-                    player.sendMessage(Util.formatMsg("&cThe monsters haven't been released yet."));
-                    player.sendMessage(Util.formatMsg("&cSee &4/dvz &cfor more info."));
-                    break;
-                }
+            }
+            if (!dvz.getGM().isStarted()) {
+                player.sendMessage(Util.formatMsg("&cThe game hasn't started yet!"));
+                break;
+            }
+            if (dvzClass.getType() == ClassType.MONSTER && !dvz.getGM().isMonsters()) {
+                player.sendMessage(Util.formatMsg("&cThe monsters haven't been released yet."));
+                player.sendMessage(Util.formatMsg("&cSee &4/dvz &cfor more info."));
+                break;
+            }
+
+            if (event.getAction() == Action.LEFT_CLICK_BLOCK || event.getAction() == Action.LEFT_CLICK_AIR) {
                 player.getInventory().clear();
                 player.updateInventory();
                 CWUtil.removeItemsFromHand(player, 1);
                 cwp.setClass(dvzClass, true);
+            } else {
+                player.sendMessage(Util.formatMsg("&cLeft click while holding the class item to select it!"));
             }
         }
     }
