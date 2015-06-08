ebf5660e3cece1b30fc952b6a313aa0b797cc928
Jos <josroossien@hotmail.com>
2015-06-08 23:27:02 +0200

    +Effect and sound for horn

--- src/com/clashwars/dvz/abilities/dwarves/Horn.java
+++ src/com/clashwars/dvz/abilities/dwarves/Horn.java
@@ -1,11 +1,13 @@
 package com.clashwars.dvz.abilities.dwarves;
 
+import com.clashwars.cwcore.packet.ParticleEffect;
 import com.clashwars.dvz.abilities.Ability;
 import com.clashwars.dvz.classes.ClassType;
 import com.clashwars.dvz.player.CWPlayer;
 import com.clashwars.dvz.util.DvzItem;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -31,11 +33,15 @@ public class Horn extends DwarfAbility {
             return;
         }
 
+        for (Player p : dvz.getServer().getOnlinePlayers()) {
+            p.playSound(p.getLocation(), Sound.ENDERDRAGON_GROWL, 1, 0);
+        }
 
         new BukkitRunnable() {
             @Override
             public void run() {
                 for (CWPlayer cwp : dvz.getPM().getPlayers(ClassType.DWARF, true, false)) {
+                    ParticleEffect.VILLAGER_ANGRY.display(0.5f, 1f, 0.5f, 0, 30, cwp.getLocation().add(0,1,0));
                     cwp.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, getIntOption("duration"), 0));
                     cwp.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, getIntOption("duration"), 0));
                 }
