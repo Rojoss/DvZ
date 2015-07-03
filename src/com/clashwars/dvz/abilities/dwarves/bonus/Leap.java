545c187c23d45e126efcf0397418b0f7ec5fe0bc
Jos <josroossien@hotmail.com>
2015-07-03 17:19:42 +0200

    %Nerf leap when there is a monster nearby.

--- src/com/clashwars/dvz/abilities/dwarves/bonus/Leap.java
+++ src/com/clashwars/dvz/abilities/dwarves/bonus/Leap.java
@@ -15,6 +15,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.util.Vector;
 
+import java.util.List;
+
 public class Leap extends BaseAbility {
 
     public Leap() {
@@ -34,10 +36,26 @@ public class Leap extends BaseAbility {
             dvz.logTimings("Leap.castAbility()[cd]", t);
             return;
         }
+
+        boolean nearbyMonster = false;
+        List<Player> players = CWUtil.getNearbyPlayers(player.getLocation(), 10);
+        for (Player p : players) {
+            if (dvz.getPM().getPlayer(p).isMonster()) {
+                nearbyMonster = true;
+                break;
+            }
+        }
+
         Vector dir = player.getLocation().getDirection();
-        player.setVelocity(player.getVelocity().add(new Vector(dir.getX() * 2.5f, 0.8f, dir.getZ() * 2.5f)));
-        player.getWorld().playSound(player.getLocation(), Sound.BAT_TAKEOFF, 0.5f, 1.5f);
-        ParticleEffect.CRIT_MAGIC.display(0.8f, 0.2f, 0.8f, 0, 30, player.getLocation());
+        if (nearbyMonster) {
+            player.setVelocity(player.getVelocity().add(new Vector(dir.getX() * 1.8f, 0.5f, dir.getZ() * 1.8f)));
+            player.getWorld().playSound(player.getLocation(), Sound.BAT_TAKEOFF, 0.25f, 1.8f);
+            ParticleEffect.CRIT_MAGIC.display(0.8f, 0.2f, 0.8f, 0, 15, player.getLocation());
+        } else {
+            player.setVelocity(player.getVelocity().add(new Vector(dir.getX() * 2.5f, 0.8f, dir.getZ() * 2.5f)));
+            player.getWorld().playSound(player.getLocation(), Sound.BAT_TAKEOFF, 0.5f, 1.5f);
+            ParticleEffect.CRIT_MAGIC.display(0.8f, 0.2f, 0.8f, 0, 30, player.getLocation());
+        }
         new AbilityDmg(player, 0, ability);
         dvz.logTimings("Leap.castAbility()", t);
     }
