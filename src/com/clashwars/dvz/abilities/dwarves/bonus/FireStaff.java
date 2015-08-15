f6992c7777d950c5e3612a148f21f2ea66ca2f01
Jos <josroossien@hotmail.com>
2015-08-15 15:09:14 +0200

    %Improve/change firestaff Now shoots fireballs rapidly with high accuracy. Can't shoot fireballs in the keep/walls anymore during dwarf time. Better sounds/effects

--- src/com/clashwars/dvz/abilities/dwarves/bonus/FireStaff.java
+++ src/com/clashwars/dvz/abilities/dwarves/bonus/FireStaff.java
@@ -1,9 +1,11 @@
 package com.clashwars.dvz.abilities.dwarves.bonus;
 
+import com.clashwars.cwcore.debug.Debug;
 import com.clashwars.cwcore.events.DelayedPlayerInteractEvent;
 import com.clashwars.cwcore.packet.ParticleEffect;
 import com.clashwars.cwcore.utils.CWUtil;
 import com.clashwars.dvz.GameState;
+import com.clashwars.dvz.abilities.Ability;
 import com.clashwars.dvz.abilities.BaseAbility;
 import com.clashwars.dvz.util.DvzItem;
 import org.bukkit.Location;
@@ -15,13 +17,15 @@ import org.bukkit.entity.Player;
 import org.bukkit.entity.SmallFireball;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
 
 public class FireStaff extends BaseAbility {
 
     public FireStaff() {
         super();
-        //ability = Ability.FIRESTAFF;
+        ability = Ability.FIRESTAFF;
         castItem = new DvzItem(Material.BLAZE_ROD, 1, (short)0, displayName, -1, -1, false);
     }
 
@@ -31,23 +35,35 @@ public class FireStaff extends BaseAbility {
             CWUtil.sendActionBar(player, CWUtil.integrateColor("&4&l>> &cThe dragon his powers are blocking you from using this right now! &4&l<<"));
             return;
         }
+        if (!dvz.getGM().isMonsters() && dvz.getMM().getActiveMap().getCuboid("keep").contains(player.getLocation())) {
+            CWUtil.sendActionBar(player, CWUtil.integrateColor("&4&l>> &cFirestaff can't be used inside the keep right now &4&l<<"));
+            return;
+        }
+        if (!dvz.getGM().isMonsters() && dvz.getMM().getActiveMap().getCuboid("innerwall").contains(player.getLocation())) {
+            CWUtil.sendActionBar(player, CWUtil.integrateColor("&4&l>> &cFirestaff can't be used inside the keep right now &4&l<<"));
+            return;
+        }
         if (onCooldown(player)) {
             return;
         }
         new BukkitRunnable() {
+            int count = 0;
             @Override
             public void run() {
-                for (int i = 0; i < 5; i++) {
+                SmallFireball smallFireball = player.launchProjectile(SmallFireball.class);
+                Vector random = new Vector(CWUtil.randomFloat(0.8f, 1.2f), CWUtil.randomFloat(0.8f, 1.2f), CWUtil.randomFloat(0.8f, 1.2f));
+                smallFireball.setVelocity(player.getEyeLocation().getDirection().multiply(random));
+                smallFireball.setMetadata("firestaff", new FixedMetadataValue(dvz, true));
 
-                    SmallFireball smallFireball = player.launchProjectile(SmallFireball.class);
-                    smallFireball.setVelocity(player.getVelocity());
+                player.getWorld().playSound(player.getLocation(), Sound.GHAST_FIREBALL, CWUtil.randomFloat(0.4f, 0.8f), CWUtil.randomFloat(0.6f, 1.2f));
+                ParticleEffect.FLAME.display(0.5f, 0.5f, 0.5f, 0, 5, player.getLocation().add(0, 1, 0));
 
-                    player.getWorld().playSound(player.getLocation(), Sound.GHAST_FIREBALL, 0.8f, 0.6f);
-                    ParticleEffect.FLAME.display(0.5f, 0.5f, 0.5f, 0, 20, player.getLocation().add(0, 1, 0));
+                count++;
+                if (count >= 8) {
+                    cancel();
                 }
-
             }
-        }.runTaskLater(dvz, 10);
+        }.runTaskTimer(dvz, 0, 2);
     }
 
     @EventHandler
@@ -57,6 +73,10 @@ public class FireStaff extends BaseAbility {
         }
 
         if(!(event.getEntity().getShooter() instanceof Player)) {
+            return;
+        }
+
+        if (!event.getEntity().hasMetadata("firestaff")) {
             return;
         }
 
@@ -72,8 +92,7 @@ public class FireStaff extends BaseAbility {
                             Block b = l.getWorld().getBlockAt(x, y, z).getRelative(BlockFace.UP);
                             if (b.getType() == Material.FIRE) {
                                 b.setType(Material.AIR);
-                                p.playSound(l, Sound.FIZZ, 1, 0.6f);
-
+                                p.playSound(l, Sound.FIZZ, 0.2f, 1.4f);
                             }
                         }
                     }
